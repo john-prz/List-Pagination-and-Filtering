a7c9fc3a195815a107bde7e2f7e1b1bbab4dc655
John Perez <45201573+john-prz@users.noreply.github.com>
2018-12-19 11:24:34 -0500

    added two additional functions

--- js/script.js
+++ js/script.js
@@ -3,41 +3,20 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+///Global variables & constants
 
-
-/***
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
-
+const divPage = document.querySelector('.page');
 const students = document.querySelectorAll('.student-item');
-const pageLimit = 9;
+const studentsPerPage = 9;
 
+//function to dynamically show or hide students based on current selected page
+const showPage = (firstStudentIndex) => {
 
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
-
-const showPage = (firstStudent) => {
-  let lastStudent = firstStudent + pageLimit;
+  let lastStudentIndex = firstStudentIndex + studentsPerPage;
 
   for(let i = 0; i < students.length; i ++) {
-    if(i < firstStudent || i > lastStudent) {
+    //add or remove the hide class based on index of students arr item
+    if(i < firstStudentIndex || i > lastStudentIndex) {
       students[i].classList.add("hide");
     }
     else {
@@ -46,15 +25,18 @@ const showPage = (firstStudent) => {
   }
 }
 
+const createPageLinks = () => {
+  
+}
 
+const appendPageLinks = () => {
+  //create div and ul that will contain pagination links
+  const div = document.createElement('div');
+  const ul = document.createElement('ul');
 
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
+  divpPage.appendChild(div);
+  div.appendChild(ul);
+  div.className = "pagination";
 
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+  createPageLinks();
+}
